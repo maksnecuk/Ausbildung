7ccccb81fa7c042870aafeee71567271d9747cd6
maks <mnetsiuk@progeek.de>
2025-11-13 10:36:50 +0100

    aufgabe 2

--- index.php
+++ index.php
@@ -1,32 +1,27 @@
 <?php
 
 class Cat {
-    public $name;
-    public $color;
-	public $age;
-	public $size;
+    public $name;//String
+    public $color; //String "black", "white", "red", " tabby"
+	public $age; //date of birth e. g. "1997-11-21"
+	public $size; //from head to the beginning of tail in cm
     public function eat() {
         echo "njom!";
     }
+    
 
 
-   /* public function getName ($name){
-        $this->name = readline("Введи ім'я кота: ");
-        
-    } */
 }
 
-
-
-
 $cat = new Cat();
-/*$cat->getName($cat);
-echo "Ім'я кота: " . $cat->name . "\n";*/
 $cat->name = "Mrs. Treble";
 $cat->color = "rot";
-$cat->age = 10;
-$cat->size = "medium";
+$cat->age = "2014-05-10";
+$cat->size = 60;
 echo "Name: $cat->name <br/> 
 Color: $cat->color<br/> 
 Age: $cat->age<br/> 
 Size: $cat->size<br/> "; 
+
+var_dump($cat->age);
+var_dump($cat->name);
